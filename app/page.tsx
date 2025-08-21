c9a39a4b097f3b3e518509b0a17e1b22f17a4146
Darren Evans <seefer@gmail.com>
2025-08-21 18:36:15 +0100

    add lusitana font and AcmeLogo component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import AcmeLogo from '@/app/ui/acme-logo';
 import styles from '@/app/ui/home.module.css';
+import { lusitana } from './ui/fonts';
 
 export default function Page() {
   return (
@@ -11,12 +12,14 @@ export default function Page() {
           'flex items-end bg-blue-500 p-4 rounded-lg h-20 md:h-52 shrink-0'
         }
       >
-        {/* <AcmeLogo /> */}
+        <AcmeLogo />
       </div>
       <div className="flex md:flex-row flex-col gap-4 mt-4 grow">
         <div className="flex flex-col justify-center gap-6 bg-gray-50 px-6 md:px-20 py-10 rounded-lg md:w-2/5">
           <div className={styles.shape}></div>
-          <p className={'text-xl text-gray-800 md:text-3xl md:leading-normal'}>
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
             <strong>Welcome to Acme!!.</strong> This is the example for the{' '}
             <a className="text-blue-500" href="https://nextjs.org/learn/">
               Next.js Learn Course
