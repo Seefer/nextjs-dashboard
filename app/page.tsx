bb7c8c03cd7ae44cc114eb70f2ed1f64ff32aa8f
Darren Evans <seefer@gmail.com>
2025-08-21 18:30:28 +0100

    added biome config and custom font

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,31 @@
-import AcmeLogo from "@/app/ui/acme-logo";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
-import Link from "next/link";
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import AcmeLogo from '@/app/ui/acme-logo';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
     <main className="flex flex-col p-6 min-h-screen">
-      <div className="flex items-end bg-blue-500 p-4 rounded-lg h-20 md:h-52 shrink-0">
+      <div
+        className={
+          'flex items-end bg-blue-500 p-4 rounded-lg h-20 md:h-52 shrink-0'
+        }
+      >
         {/* <AcmeLogo /> */}
       </div>
       <div className="flex md:flex-row flex-col gap-4 mt-4 grow">
         <div className="flex flex-col justify-center gap-6 bg-gray-50 px-6 md:px-20 py-10 rounded-lg md:w-2/5">
-          <p className={"text-xl text-gray-800 md:text-3xl md:leading-normal"}>
-            <strong>Welcome to Acme.</strong> This is the example for the{" "}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+          <div className={styles.shape}></div>
+          <p className={'text-xl text-gray-800 md:text-3xl md:leading-normal'}>
+            <strong>Welcome to Acme!!.</strong> This is the example for the{' '}
+            <a className="text-blue-500" href="https://nextjs.org/learn/">
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
             className="flex items-center self-start gap-5 bg-blue-500 hover:bg-blue-400 px-6 py-3 rounded-lg font-medium text-white text-sm md:text-base transition-colors"
+            href="/login"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
