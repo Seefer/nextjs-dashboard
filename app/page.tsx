aa32886557614821bcdcb6306f982923f3e0d16e
Darren Evans <seefer@gmail.com>
2025-08-22 15:13:42 +0100

    added prettierignore. Add prettier check and format scripts

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme!!.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme!</strong> This is the example for the{' '}
             <a className="text-blue-500" href="https://nextjs.org/learn/">
               Next.js Learn Course
             </a>
